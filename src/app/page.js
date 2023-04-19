2f6b519c7ae61f641e215f9aa9b3f4dd9b7dde37
lyovson <lyovson@icloud.com>
2023-04-19 22:25:01 +0400

    added crew section polish

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 import About from "../components/About.js";
 import Crew from "../components/Crew.js";
-import Divider from "../components/Divider.jsx";
 import Footer from "../components/Footer.js";
 import Header from "../components/Header.js";
 import Latest from "../components/Latest.js";
-import Projects from "../components/Projects.js";
 
 export default function Home() {
   return (
