d7e7d0a3baf670f39532888ea48c6c5b1daef924
lyovson <lyovson@icloud.com>
2023-04-28 14:29:59 +0400

    added signature logo

--- src/app/[locale]/resources/page.js
+++ src/app/[locale]/resources/page.js
@@ -8,9 +8,14 @@ export default function Resources() {
         <img src="/resources-poster.jpg" alt="OTTK Film Poster" />
         <img
           src="/resources-logo-old.png"
-          alt="Hooligun Studio Cinematic Logo"
+          alt="Hooligun Studios Cinematic Logo"
         />
-        <img src="/resources-logo-new.png" alt="Hooligun Studio Brand Logo" />
+        <img src="/resources-logo-new.png" alt="Hooligun Studios Brand Logo" />
+        <img
+          src="/signatureLogo.png"
+          alt="Hooligun Studios Signature Logo"
+          className="hidden"
+        />
       </main>
     </section>
   );
