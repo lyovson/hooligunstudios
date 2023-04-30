0919f9c439dcf01ad60585a4d047cbcc7c9c9d35
lyovson <lyovson@icloud.com>
2023-05-01 01:52:09 +0400

    added pass protection

--- src/app/[locale]/projects/donkey/layout.js
+++ src/app/[locale]/projects/donkey/layout.js
@@ -1,6 +1,43 @@
+"use client";
+
+import { useState } from "react";
+
 import Menu from "./Menu.jsx";
 
 export default function ProjectLayout({ children }) {
+  const [value, setValue] = useState("");
+  const [logged, setLogged] = useState(false);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onEnter = (e) => {
+    if (e.key === "Enter") {
+      if (value === "TopSecret!") {
+        setLogged(true);
+      } else {
+        setValue("");
+      }
+    }
+  };
+
+  if (!logged) {
+    return (
+      <section className="m-16 flex flex-col gap-2">
+        <label htmlFor="password">Password:</label>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          onKeyDown={onEnter}
+          id="password"
+          name="password"
+          className="w-full border-2 border-stone-900 bg-transparent p-4 outline-none"
+        />
+      </section>
+    );
+  }
   return (
     <section className={` relative flex flex-col lg:flex-row`}>
       <section className="max-w-[90%]">{children}</section>
