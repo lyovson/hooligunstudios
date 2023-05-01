a201ddff39433e3c7eb2d40f7a2b725e8eb940ec
lyovson <lyovson@icloud.com>
2023-05-01 04:39:14 +0400

    changed layouts

--- src/app/[locale]/projects/donkey/SquareCard.jsx
+++ src/app/[locale]/projects/donkey/SquareCard.jsx
@@ -15,7 +15,7 @@ export default function Card({ info }) {
       variants={cardVariants}
       whileHover="hover"
       initial="initial"
-      className={` flex max-w-[300px] flex-col items-center rounded-xl bg-stone-900 bg-opacity-80 shadow-md md:max-w-[800px]  md:flex-row md:justify-center `}
+      className={` flex max-w-[300px] flex-col items-center rounded-xl bg-stone-900 bg-opacity-80 shadow-md md:max-w-[800px]  md:flex-row md:items-start md:justify-center`}
     >
       <Image
         className={` rounded-t-xl md:rounded-l-xl md:rounded-r-none`}
@@ -25,9 +25,20 @@ export default function Card({ info }) {
         alt={info.slug}
       />
       <aside className=" flex flex-col gap-8  p-4 ">
-        <article className=" flex flex-col">
+        <article className=" flex flex-col gap-2">
           <h3 className={`font-russian text-2xl font-bold`}>{info.name}</h3>
-          <p className=" text-base font-extralight">{info.role}</p>
+          <p className="italic">{info.role}</p>
+          <p className=" text-base font-extralight">{info.bio}</p>
+          {info.approved && (
+            <p className=" my-8 w-[120px]  bg-green-700 p-2 text-center text-xs text-black ">
+              Подтверждено
+            </p>
+          )}
+          {!info.approved && (
+            <p className=" my-8 w-[120px]  bg-yellow-700 p-2 text-center text-xs text-black ">
+              В Переговорах
+            </p>
+          )}
         </article>
       </aside>
     </motion.article>
