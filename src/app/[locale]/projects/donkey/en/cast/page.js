f53dcfa1c242ec6697ce20f763975feb83589487
lyovson <lyovson@icloud.com>
2023-05-03 13:30:20 +0400

    added english treatment

---
+++ src/app/[locale]/projects/donkey/en/cast/page.js
@@ -0,0 +1,51 @@
+"use client";
+
+import SquareCard from "../SquareCard.jsx";
+
+export default function Cast() {
+  return (
+    <main className="mb-12 flex   flex-col gap-4 p-8">
+      <h2 className="py-4 text-left font-russian text-2xl font-bold">
+        Prime Cast:
+      </h2>
+      <section className="flex  flex-col items-center justify-center gap-4">
+        <SquareCard
+          info={{
+            name: "Narek A Chilla",
+            bio: "Famous rapper, actor. Has a devoted fanbase. Especially popular among young people. His latest songs have millions of views.",
+            image: "/donkey/chilla.png",
+            approved: true,
+            role: "Father - Victor",
+          }}
+        />
+        <SquareCard
+          info={{
+            name: "Iveta Mukuchyan",
+            bio: "Beloved singer, actress. Occupies leading positions in Armenian show business for many years.",
+            image: "/donkey/iveta.png",
+            approved: false,
+            role: "Mother - Lara",
+          }}
+        />
+        <SquareCard
+          info={{
+            name: "Arthur Babich",
+            bio: "Famous blogger, Tik-Toker from Russia. Has more than 14 million followers.",
+            image: "/donkey/babich.png",
+            approved: false,
+            role: "Boyfriend - Mark",
+          }}
+        />
+        <SquareCard
+          info={{
+            name: "Milena Mikaelyan",
+            bio: "Resident of Hooligun Studios, talented young actress, singer.",
+            image: "/donkey/milena.png",
+            approved: true,
+            role: "Daughter - Nika",
+          }}
+        />
+      </section>
+    </main>
+  );
+}
---
+++ src/app/[locale]/projects/donkey/en/media/page.js
@@ -0,0 +1,48 @@
+import Video from "../../../../../../components/Video.jsx";
+
+export default function Media() {
+  return (
+    <main className="mb-12 flex   flex-col gap-4 p-8 ">
+      <h2 className="py-4 text-left font-russian text-2xl font-bold">
+        Musical Reference:
+      </h2>
+      <sections className="flex flex-row flex-wrap gap-4">
+        <Video
+          className={` max-w-[350px]`}
+          videoId={"lelMnvpaf0s"}
+          title={`Romance in the Clouds`}
+          info={`How To Train Your Dragon OST`}
+        />
+        <Video
+          className={` max-w-[350px]`}
+          videoId={"MIvWZ8iF6CI"}
+          title={`Jennifer Connely - Sway`}
+          info={`Dark City OST`}
+        />
+        <Video
+          className={` max-w-[350px]`}
+          videoId={"TQV-0fwwCQM"}
+          title={`One T + Cool T - The Magic Key`}
+          info={`A great song from '00s`}
+        />
+      </sections>
+      <h2 className="py-4 text-left font-russian text-2xl font-bold">
+        Visual Reference:
+      </h2>
+      <sections className="flex flex-row flex-wrap gap-4">
+        <Video
+          className={` max-w-[350px]`}
+          videoId={"ecE0ZeNlejE"}
+          title={`Camera Reference`}
+          info={` Arri Alexa Mini Kodak 2383 Film Emulation`}
+        />
+        <Video
+          className={` max-w-[350px]`}
+          videoId={"Ltba1_-tRT4"}
+          title={`Color Reference`}
+          info={`Night at the Museum Trailer`}
+        />
+      </sections>
+    </main>
+  );
+}
